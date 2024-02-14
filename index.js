639f622631c5f14c78e79677d66712daff623955
vizucode <havisikkubaru@gmail.com>
2024-02-15 00:30:31 +0700

    feat: bot

--- index.js
+++ index.js
@@ -1,44 +1,48 @@
-import express from 'express';
 import axios from 'axios';
 import * as cheerio from 'cheerio';
+import dotenv from 'dotenv';
+import TelegramBot from 'node-telegram-bot-api';
 
-const app = express()
+const env = dotenv.config()
 
 const base_url = "https://pemilu.kompas.com/quickcount/indikator"
+const baseHTML = await axios.get(base_url)
+const $ = cheerio.load(baseHTML.data)
 
-app.get("/quickcount", async (req, res) => {
-    const baseHTML = await axios.get(base_url)
-    const $ = cheerio.load(baseHTML.data)
-
-    var anis = 0
-    var owo = 0
-    var gunjar = 0
-    var counterList = $(".qcResult__num").each(function (i, elem) {
-        var cleanedString = $(this).text().replace(/\s/g, '-');
-        var numbersArray = cleanedString.match(/[\d.]+/g);
-        anis = numbersArray[0]
-        owo = numbersArray[1]
-        gunjar = numbersArray[2]
-    })
-
-
-    var lastUpdedList = []
-    var lastUpdated = $(".qcResult__vote__item").find("span").each(function (i, elem) {
-        lastUpdedList.push($(this).text())
-    })
-
-    res.json({ 
-        "member": {
-            "anis": anis,
-            "prabowo": owo,
-            "ganjar": gunjar
-        },
-        "last_updated": lastUpdedList[3],
-        "last_data_entry": lastUpdedList[4]
-    })
+var anis = 0
+var owo = 0
+var gunjar = 0
+var counterList = $(".qcResult__num").each(function (i, elem) {
+    var cleanedString = $(this).text().replace(/\s/g, '-');
+    var numbersArray = cleanedString.match(/[\d.]+/g);
+    anis = numbersArray[0]
+    owo = numbersArray[1]
+    gunjar = numbersArray[2]
 })
 
 
-app.listen(3000, () => {
-    console.log("running on port 3000")
+var lastUpdedList = []
+var lastUpdated = $(".qcResult__vote__item").find("span").each(function (i, elem) {
+    lastUpdedList.push($(this).text())
 })
+
+
+const token = process.env.TOKEN; // Replace with your own bot token
+const bot = new TelegramBot(token, { polling: true });
+
+var count = 0
+setInterval(() => {
+    count++
+    bot.editMessageText(`
+[ Pilpres Quickcount ]
+
+Anis: ${anis}
+Prabowo: ${owo}
+Ganjar: ${gunjar}
+
+Last Updated: ${lastUpdedList[3]}
+Data Masuk Terakhir: ${lastUpdedList[4]}
+
+count: ${count}
+`, { chat_id: -1002050923780, message_id: 2, parse_mode: 'Markdown' })
+}, 5000);
